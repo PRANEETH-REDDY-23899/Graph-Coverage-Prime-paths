195a57ddccd1c1a296b1d10f696bc49ae7c4f494
Praneeth22 <yennampraneeth@gmail.com>
2024-03-21 18:56:50 -0400

    error: changing the layout code

--- main.py
+++ main.py
@@ -3,13 +3,55 @@ import graph_utils
 import graph_coverage
 
 def main():
-    # st.title("Graph Coverage Prime Paths")
-    st.sidebar.title("Navigation")
-    page = st.sidebar.radio("Select a page:", ["Home", "Test Path"])
+    st.markdown(
+        """
+        <style>
+            .navbar {
+                overflow: hidden;
+                background-color: #333;
+                position: fixed;
+                top: 0;
+                width: 100%;
+            }
 
-    if page == "Home":
+            .navbar a {
+                float: left;
+                display: block;
+                color: #f2f2f2;
+                text-align: center;
+                padding: 14px 20px;
+                text-decoration: none;
+                font-size: 17px;
+            }
+
+            .navbar a:hover {
+                background-color: #ddd;
+                color: black;
+            }
+
+            .content {
+                margin-top: 50px; /* Adjust this value based on navbar height */
+            }
+        </style>
+        """
+    , unsafe_allow_html=True)
+
+    st.markdown(
+        """
+        <div class="navbar">
+            <a href="#home">Home</a>
+            <a href="#test_path">Test Path</a>
+        </div>
+        """
+    , unsafe_allow_html=True)
+
+    st.title("Graph Coverage Prime Paths")
+    st.markdown("<div class='content'>", unsafe_allow_html=True)
+
+    # Page content
+    if 'home' in st.session_state:
         graph_utils.display_home_page()
-    elif page == "Test Path":
+    elif 'test_path' in st.session_state:
         graph_coverage.display_test_path_page()
 
 if __name__ == "__main__":
